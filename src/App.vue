76aa8e966aa3aed77769cb2ea8aefdda12d76947
gldaniil <daniil.gajfullin@yandex.ru>
2025-05-30 10:30:26 +0500

    fix: remove unused styles

--- src/App.vue
+++ src/App.vue
@@ -9,32 +9,3 @@ import AppSidebar from './components/AppSidebar.vue';
     <AppContent />
   </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
